38c88b5180d127035f26795f723e277a09eae8f9
Esteban Leiva <e.leivam@uniandes.edu.co>
2021-02-02 19:33:31 -0500

    Entrega Final –laboratorio 1

--- App/view.py
+++ App/view.py
@@ -42,7 +42,7 @@ def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TO-DO: Modificaciones para completar el laboratorio 1.
+    print("3- Cargar Books-tags")
     print("0- Salir")
 
 
@@ -76,6 +76,8 @@ while True:
         tags = loadTags()
         print('Total de tags cargados: ' + str(lt.size(tags)))
     # TO-DO: Modificaciones para completar el laboratorio 1.
+    elif int(inputs[0]) == 3:
+        print("Cargando información de Books-tags....")
 
     else:
         sys.exit(0)
